8a8b123b4a8be6e6371005dd5329460eb823abd0
shahd <shoadell9@gmail.com>
2024-10-15 16:07:51 +0300

    initial home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = "Home" ;
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
